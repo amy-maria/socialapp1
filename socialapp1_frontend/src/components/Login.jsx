3e8458cae13bfc5e108497f501942f75c3cc99d5
amy-maria <122695916+amy-maria@users.noreply.github.com>
2023-06-15 12:00:14 -0400

    corrected Google auth and login

--- socialapp1_frontend/src/components/Login.jsx
+++ socialapp1_frontend/src/components/Login.jsx
@@ -1,77 +1,69 @@
 import React from "react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
-import { FaGoogle } from "react-icons/fa";
 import logo from "../assets/logowhite.png";
 import shareVideo from "../assets/share.mp4";
 import { client } from "../client";
 import { GoogleLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
 
 const Login = () => {
   const navigate = useNavigate();
-  const responseGoogle = (response) => {
-    localStorage.setItem("user", JSON.stringify(response.profileObj));
-    const { name, googleId, imageUrl } = response.profileObj;
+  const createOrGetUser = async (response) => {
+    const decoded = jwt_decode(response.credential);
+    localStorage.setItem("user", JSON.stringify(decoded));
+
+    const { name, picture, sub } = decoded;
     const doc = {
-      _id: googleId,
+      _id: sub,
       _type: "user",
       userName: name,
-      image: imageUrl,
+      image: picture,
     };
+
     client.createIfNotExists(doc).then(() => {
       navigate("/", { replace: true });
     });
   };
 
   return (
-    <div className="flex justify-start item-center flex-col h-screen">
-      <div className="relative w-full h-full">
-        <video
-          src={shareVideo}
-          type="video/mp4"
-          loop
-          controls={false}
-          muted
-          autoPlay
-          className="
+    <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}>
+      <div className="flex justify-start item-center flex-col h-screen">
+        <div className="relative w-full h-full">
+          <video
+            src={shareVideo}
+            type="video/mp4"
+            loop
+            controls={false}
+            muted
+            autoPlay
+            className="
       
         w-full
         h-full object-cover"
-        />
-        <div className="absolute flex flex-col justify-center items-center top-0 bottom-0 left-0 right-0 bg-blackOverlay">
-          <div className="p-5">
-            <img src={logo} width="130px" alt="logo" />
-            <div className="shadow-2xl">
-              <GoogleOAuthProvider
-                clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
-              >
-                render=
-                {(renderProps) => (
-                  <button
-                    type="button"
-                    className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                    onClick={renderProps.onClick}
-                    disabled={renderProps.disabled}
-                  >
-                    Sign in with Google
-                  </button>
-                )}
-                <div className="GoogleLogin">
-                  <GoogleLogin
-                    onSuccess={(responseGoogle) => {
-                      console.log(responseGoogle);
-                    }}
-                    onFailure={(responseGoogle) => {
-                      console.log("Login Failed");
-                    }}
-                  />
-                </div>
-              </GoogleOAuthProvider>
+          />
+          <div className="absolute flex flex-col justify-center items-center top-0 bottom-0 left-0 right-0 bg-blackOverlay">
+            <div className="p-5">
+              <img src={logo} width="130px" alt="logo" />
+              <div className="shadow-2xl">
+                <GoogleLogin
+                  onSuccess={(response) => createOrGetUser(response)}
+                  onError={() => console.log("Error")}
+                  cookiePolicy="single_host_origin"
+                />
+
+                {/* {user ? (<div>Logged In</div>)
+                                : <GoogleLogin
+                                    onSuccess={(response) => createOrGetuser(response)}
+                                    onError={() => console.log('Error')}
+                                />
+                            } */}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 };
 
